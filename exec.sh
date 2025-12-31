142ad86bc25db16becfc963d57cb5afc9a6a9bd8
cypress-yang <cy706.yang@gmail.com>
2025-12-31 09:38:53 +0000

    add bs_roformer sep

--- exec.sh
+++ exec.sh
@@ -3,24 +3,24 @@ git submodule update --init --recursive
 # pip uninstall flash-attn
 
 cd thirdparty/SongFormer/src/SongFormer
-# {
-#     python utils/fetch_pretrained.py
+{
+    python utils/fetch_pretrained.py
     
-#     export PYTHONPATH=../third_party:$PYTHONPATH
-#     export OMP_NUM_THREADS=1
-#     export MPI_NUM_THREADS=1
-#     export NCCL_P2P_DISABLE=1
-#     export NCCL_IB_DISABLE=1
+    export PYTHONPATH=../third_party:$PYTHONPATH
+    export OMP_NUM_THREADS=1
+    export MPI_NUM_THREADS=1
+    export NCCL_P2P_DISABLE=1
+    export NCCL_IB_DISABLE=1
 
-#     python infer/infer.py \
-#     -i /mnt/chenyuyang/AutoPrepSongV2/test.scp \
-#     -o /mnt/chenyuyang/AutoPrepSongV2/local/test_SongFormer_output \
-#     --model SongFormer \
-#     --checkpoint SongFormer.safetensors \
-#     --config_path SongFormer.yaml \
-#     -gn 8 \
-#     -tn 1
-# }
+    python infer/infer.py \
+    -i /mnt/chenyuyang/AutoPrepSongV2/test.scp \
+    -o /mnt/chenyuyang/AutoPrepSongV2/local/test_SongFormer_output \
+    --model SongFormer \
+    --checkpoint SongFormer.safetensors \
+    --config_path SongFormer.yaml \
+    -gn 8 \
+    -tn 1
+}
 
 cd ../../../../
 
